aa38f967517b49750d5a647ad37816bb82a0e483
edas112 <you@example.com>
2024-04-09 21:22:38 +0300

    sasss

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Sass } from 'scss';
 import './variables.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
