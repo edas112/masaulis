4eae0be50a6a8f4360d2f399ade31bb6962456b1
edas112 <you@example.com>
2024-04-09 23:09:24 +0300

    IMG import

--- src/components/Galerija/Titulinis/Titulinis.jsx
+++ src/components/Galerija/Titulinis/Titulinis.jsx
@@ -1,5 +1,14 @@
 import { Carousel } from 'react-bootstrap';
 import Img from '../../../assets/img/321428278_726151968599493_3016709172147485044_n.jpg';
+import Img2 from '../../../assets/img/322252237_5401922049911849_343718617657941608_n.jpg';
+import Img3 from '../../../assets/img/322878445_685586683234553_8194654702181948721_n.jpg';
+import Img4 from '../../../assets/img/322297695_547332113961748_3551942094303599329_n.jpg';
+import Img5 from '../../../assets/img/322878445_685586683234553_8194654702181948721_n.jpg';
+import Img6 from '../../../assets/img/322297695_547332113961748_3551942094303599329_n.jpg';
+import Img7 from '../../../assets/img/322252237_5401922049911849_343718617657941608_n.jpg';
+import Img8 from '../../../assets/img/322297695_547332113961748_3551942094303599329_n.jpg';
+import Img9 from '../../../assets/img/322252237_5401922049911849_343718617657941608_n.jpg';
+
 function Titulinis() {
   return (
     <div>
@@ -13,13 +22,13 @@ function Titulinis() {
           />
           <img
             className="container"
-            src="../src/assets/img/322252237_5401922049911849_343718617657941608_n.jpg"
+            src={Img2}
             alt="Second slide"
             style={{ width: '33%' }}
           />
           <img
             className="container"
-            src="../src/assets/img/322878445_685586683234553_8194654702181948721_n.jpg"
+            src={Img3}
             alt="Third slide"
             style={{ width: '33%' }}
           />
@@ -31,19 +40,19 @@ function Titulinis() {
         <Carousel.Item>
           <img
             className="container"
-            src="../src/assets/img/322297695_547332113961748_3551942094303599329_n.jpg"
+            src={Img4}
             alt="First slide"
             style={{ width: '33%' }}
           />
           <img
             className="container"
-            src="../src/assets/img/321207935_931781768170726_15131688440972736_n.jpg"
+            src={Img5}
             alt="Second slide"
             style={{ width: '33%' }}
           />
           <img
             className="container"
-            src="../src/assets/img/321580656_1315347745983668_4073054583856034283_n.jpg"
+            src={Img6}
             alt="Third slide"
             style={{ width: '33%' }}
           />
@@ -54,19 +63,19 @@ function Titulinis() {
         <Carousel.Item>
           <img
             className="container"
-            src="../src/assets/img/IMG_20231117_095157.jpg"
+            src={Img7}
             alt="First slide"
             style={{ width: '33%' }}
           />
           <img
             className="container"
-            src="../src/assets/img/322129826_898660871566325_7115684282536262059_n.jpg"
+            src={Img8}
             alt="Second slide"
             style={{ width: '33%' }}
           />
           <img
             className="container"
-            src="../src/assets/img/IMG_20240227_145526.jpg"
+            src={Img9}
             alt="Third slide"
             style={{ width: '33%' }}
           />
