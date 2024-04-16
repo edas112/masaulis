b9e37d68e6b51571b07d73de843e9adb51f29765
edas112 <you@example.com>
2024-04-16 20:00:09 +0300

    vercel conecnt

--- src/cfg/cfg.js
+++ src/cfg/cfg.js
@@ -2,7 +2,7 @@ export const cfg = {
   API: {
     HOST:
       process.env.NODE_ENV === 'production'
-        ? 'https://api-masaulis-five.vercel.app/'
+        ? 'https://api-masaulis-five.vercel.app'
         : 'http://localhost:3000',
   },
 };
