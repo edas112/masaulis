b03303618057261fbfcffa25e7e72302fd0ef5b9
edas112 <you@example.com>
2024-05-02 20:22:58 +0300

    stiliai

--- src/components/Navigacija/Navigacija.jsx
+++ src/components/Navigacija/Navigacija.jsx
@@ -12,7 +12,6 @@ import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { useState } from 'react';
-import Logo from './Logo/Logo';
 
 function Navigacija() {
   const [show, setShow] = useState(false);
@@ -41,9 +40,7 @@ function Navigacija() {
       </div>
       <Navbar expand="lg" className="nav">
         <Navbar.Brand>
-          <h1 className="logo">
-            <Logo />
-          </h1>
+          <h1 className="logo">Masaulis.lt</h1>
         </Navbar.Brand>
         <Navbar.Toggle
           className="nav-toggle"
--- src/components/Pagrindinis/Pagrindinis.jsx
+++ src/components/Pagrindinis/Pagrindinis.jsx
@@ -10,12 +10,11 @@ function Pagrindinis() {
       <div className="conteiner-pagrindinis container">
         <h1>MB Masaulis</h1>
         <p>
-          Profesionalų komanda atliekanti visus statybos, remonto, apdailos,
-          elektros instaliacijos darbus! <br /> VISUS DARBUS ATLIEKAME
-          KOKYBIŠKAI IR LAIKANTIS TERMINŲ! <br /> Bendradarbiaujame su
-          profesonaliais architektais, projektuotojais, dizaineriais,
-          santechnikais, baldų bei durų gamintojais. Naudojame tik naujausius ir
-          kokybiškus įrankius bei techniką. Galime pasirūpinti Jūsų projektų nuo
+          Profesionalų komanda atliekanti visus apdailos darbus! <br /> VISUS
+          DARBUS ATLIEKAME KOKYBIŠKAI IR LAIKANTIS TERMINŲ! <br />{' '}
+          Bendradarbiaujame su profesonaliais architektais, projektuotojais,
+          dizaineriais, santechnikais, baldų bei durų gamintojais. Naudojame
+          šiuolaikiškus darbo įrankius. Galime pasirūpinti Jūsų projektų nuo
           pradžios iki pabaigos! Visų darbų kainos KONKURENCINGOS!!
         </p>
       </div>
