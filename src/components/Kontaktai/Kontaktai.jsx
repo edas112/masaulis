fc27750a8867d7d51694d05458bc8e4f21077ae2
edas112 <you@example.com>
2024-05-02 23:01:25 +0300

    baigstas

--- src/components/Kontaktai/Kontaktai.jsx
+++ src/components/Kontaktai/Kontaktai.jsx
@@ -5,13 +5,16 @@ import { AppContext } from '../../context/AppContext';
 import './kontaktai.scss';
 
 function Kontaktai() {
-  const { footerData } = useContext(AppContext);
+  const { footerData, loadingFooterData } = useContext(AppContext);
 
   return (
     <>
       <EmailLink />;
       <div className="kontaktai container">
         <h1>Kontaktai</h1>
+        {loadingFooterData && !footerData.length && (
+          <h2 className="kraunasi"></h2>
+        )}
         <p>{footerData.title}</p>
         <p>{footerData.adres}</p>
         <p>{footerData.email}</p>
